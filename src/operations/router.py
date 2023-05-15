d580ecfa149cc69666008360273e734be50149b1
TimaDootaliev <timur.dootaliev.td@gmail.com>
2023-05-15 14:20:17 +0600

    redis

--- src/operations/router.py
+++ src/operations/router.py
@@ -16,16 +16,17 @@ router = APIRouter(
 
 @router.get("/", response_model=List[Operation])
 async def get_specific_operations(
-        operation_type: str, 
+        operation_type: str,
         session: AsyncSession = Depends(get_async_session)):
-    query = select('*').select_from(operation)\
-        .where(operation.c.type == operation_type)
+    query = select(operation).where(operation.c.type == operation_type)
     result = await session.execute(query)
     return result.all()
 
 
 @router.post("/")
-async def add_specific_operations(new_operation: OperationCreate, session: AsyncSession = Depends(get_async_session)):
+async def add_specific_operations(
+            new_operation: OperationCreate,
+            session: AsyncSession = Depends(get_async_session)):
     stmt = insert(operation).values(**new_operation.dict())
     await session.execute(stmt)
     await session.commit()
